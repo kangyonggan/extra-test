994dba322bc7033816bac194078cb2ccf8918187
kangyonggan <kangyonggan@gmail.com>
2017-11-07 18:00:34 +0800

    test @Valid.java

---
+++ src/main/java/com/kangyonggan/extra/Demo10.java
@@ -0,0 +1,25 @@
+package com.kangyonggan.extra;
+
+
+import com.kangyonggan.extra.annotation.Valid;
+
+/**
+ * @author kangyonggan
+ * @since 11/7/17
+ */
+public class Demo10 {
+
+    @Valid(interrupt = true)
+    public static void login(User user) {
+        System.out.println(user);
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("asdasdasdasd");
+        user.setPassword("1234@5678");
+        login(user);
+    }
+
+}
